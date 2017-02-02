dc39f01fb3415383f411406d2f8e1c15ab6b58d5
acharekart <acharekart@gmail.com>
2017-02-02 11:24:20 -0500

    Polymer started

--- assetes/js/overideDefault.js
+++ assetes/js/overideDefault.js
@@ -1,34 +1,15 @@
-$(document).ready(function(){
+$(document).ready(function(){	
 
-var finalHeight = $(".navbar-fixed-top").position().top + $(".navbar-fixed-top").outerHeight(); 
-var screenWidth = screen.width;
+	adjustment();
 
-if(screenWidth <= 767){
-	$(".mainBody").css({"position": "relative", "top":finalHeight-25});
-}
-else{
-	$(".mainBody").css({"position": "relative", "top":finalHeight});
-	$(".leftPanel").css({"position": "relative", "top":finalHeight});
-}
-		
-    $(".leftPanel .nav-pills li").click(function(ev){
-		$(".leftPanel .nav-pills li").removeClass("active");
-        $(this).addClass("active");
-		ev.stopPropagation();
-    }); 
-	
-	$(".hideDesktop").click(function(ev){
-		if (parseFloat($(".connect_leftMenuMob").css("left")) < -1 ){
-			$(".connect_leftMenuMob").animate({left: '-1px'});
-			}
-        else{$(".connect_leftMenuMob").animate({left: '-75%'});} 
-		ev.stopPropagation();
-    });
-	
-	$("body").click(function(ev){
-		if (parseFloat($(".connect_leftMenuMob").css("left")) == -1 ){
-			$(".connect_leftMenuMob").animate({left: '-75%'});
-		}
-		ev.stopPropagation();
-    });	
+	$(window).resize(function() {adjustment();});
 });
+
+
+function adjustment(){
+/* 	var finalHeight = $(".main_header").position().top + $(".main_header").outerHeight(); 
+	var screenWidth = screen.width;
+ 	$(".mainBody").css({"position": "relative", "top":finalHeight}); 
+	$(".mdl-layout__drawer").css({"position": "relative", "top":finalHeight}); */
+}
+
